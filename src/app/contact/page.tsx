c425c744295dd57ea26ecb5ab60c495b2f6182bf
CTO - LAIBA NAZ <id.laibanaz@gmail.com>
2025-01-06 22:19:51 +0500

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,6 @@
 export default function Contact(){
    return(
-      <div className="pt-96 font-bold text-center text-purple-800 text-2xl">
+      <div className="py-80 font-bold text-center text-purple-800 text-2xl">
             <p>We are working on this page!</p>
       </div>
    );
